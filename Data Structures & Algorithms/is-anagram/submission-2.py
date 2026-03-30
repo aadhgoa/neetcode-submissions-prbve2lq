b8b4e72a3076ab9b7c7744a6d1a42577cf57de34
Aayush Anand <53388828+aadhgoa@users.noreply.github.com>
2026-03-30 23:09:22 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        map_char_s = defaultdict(int)
+        map_char_t = defaultdict(int)
+
+        for char in s:
+            map_char_s[char] += 1
+
+        for char in t:
+            map_char_t[char] += 1
+
+        return map_char_s == map_char_t
+            
